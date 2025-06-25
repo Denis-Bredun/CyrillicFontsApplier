a4917b372cc0e0bf61aca81d08b93579411fa56c
Monkey D Luffy <bredun.denis@gmail.com>
2025-06-25 10:32:59 +0300

    Added fonts configuration

--- CyrillicFontsApplier.Client/MauiProgram.cs
+++ CyrillicFontsApplier.Client/MauiProgram.cs
@@ -7,19 +7,25 @@ namespace CyrillicFontsApplier.Client
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+            builder.UseMauiApp<App>();
+            ConfigureFonts(builder);
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
+        }
+
+        private static void ConfigureFonts(MauiAppBuilder builder)
+        {
+            builder.ConfigureFonts(fonts =>
+            {
+                foreach (var font in Constants.FontList.Fonts)
+                {
+                    fonts.AddFont(font.Key, font.Value);
+                }
+            });
         }
     }
 }
