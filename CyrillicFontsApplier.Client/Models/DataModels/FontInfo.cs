d6270b0e87a8812bb929a3371b67396b07ed976b
Monkey D Luffy <bredun.denis@gmail.com>
2025-06-25 11:23:16 +0300

    Added FontInfo data model

---
+++ CyrillicFontsApplier.Client/Models/DataModels/FontInfo.cs
@@ -0,0 +1,15 @@
+﻿using CyrillicFontsApplier.Client.Models.Enums;
+
+namespace CyrillicFontsApplier.Client.Models.DataModels
+{
+    public class FontInfo
+    {
+        public string Name { get; set; }
+        public string FileName { get; set; }
+        public string LocalPath { get; set; }
+        public FontOrigin Origin { get; set; }
+        public bool IsFavorite { get; set; }
+        public DateTime? LastUsed { get; set; }
+        public DateTime? DownloadedAt { get; set; }
+    }
+}
