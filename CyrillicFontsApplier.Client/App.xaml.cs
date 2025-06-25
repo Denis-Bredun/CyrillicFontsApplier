bc81518c8ad30b9b38c1153cd9261cb43c5ef0f1
Monkey D Luffy <bredun.denis@gmail.com>
2025-06-25 08:34:35 +0300

    Add project files.

---
+++ CyrillicFontsApplier.Client/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CyrillicFontsApplier.Client
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CyrillicFontsApplier.Client/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CyrillicFontsApplier.Client
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
