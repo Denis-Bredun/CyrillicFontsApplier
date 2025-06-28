05158b499d2d27cde112b3889f9ace0a2df20cbd
denis.bredun <bredun.denis@gmail.com>
2025-06-29 01:01:07 +0300

    Added FontFavoritesJsonConverter

---
+++ CyrillicFontsApplier.Client/Interfaces/IDictionaryToTextViewConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyrillicFontsApplier.Client.Interfaces
+{
+    public interface IDictionaryToJsonConverter<TKey, TValue> where TKey : notnull
+    {
+        string Serialize(Dictionary<TKey, TValue> dictionary);
+        Dictionary<TKey, TValue> Deserialize(string json);
+    }
+}
---
+++ CyrillicFontsApplier.Client/Services/FontFavoritesJsonConverter.cs
@@ -0,0 +1,25 @@
+﻿using CyrillicFontsApplier.Client.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CyrillicFontsApplier.Client.Services
+{
+    public class FontFavoritesJsonConverter : IDictionaryToJsonConverter<string, bool>
+    {
+        private JsonSerializerOptions _serializerOptions = new JsonSerializerOptions
+        {
+            AllowTrailingCommas = true,
+            WriteIndented = true
+        };
+
+        public string Serialize(Dictionary<string, bool> fontsFavoriteStates)
+            => JsonSerializer.Serialize(fontsFavoriteStates, _serializerOptions);
+
+        public Dictionary<string, bool> Deserialize(string json)
+            => JsonSerializer.Deserialize<Dictionary<string, bool>>(json);
+    }
+}
