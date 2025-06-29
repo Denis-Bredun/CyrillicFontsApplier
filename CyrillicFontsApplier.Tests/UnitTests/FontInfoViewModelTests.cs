133948bd4fa947df21c92315a9b30b0b1382b349
denis.bredun <bredun.denis@gmail.com>
2025-06-29 17:44:35 +0300

    Made a test for FontInfoViewModel for checking the work of logic

---
+++ CyrillicFontsApplier.Tests/UnitTests/FontInfoViewModelTests.cs
@@ -0,0 +1,29 @@
+﻿using CyrillicFontsApplier.Client.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyrillicFontsApplier.Tests.UnitTests
+{
+    public class FontInfoViewModelTests
+    {
+        private const string TestFilename = "Alegreya-Regular.ttf";
+        private const string ExpectedAlias = "AlegreyaRegular";
+        private const string ExpectedDisplayedName = "Alegreya";
+
+        [Fact]
+        public void Constructor_ShouldInitializePropertiesCorrectly()
+        {
+            // Act
+            var viewModel = new FontInfoViewModel(TestFilename);
+
+            // Assert
+            Assert.Equal(TestFilename, viewModel.Filename);
+            Assert.Equal(ExpectedAlias, viewModel.Alias);
+            Assert.Equal(ExpectedDisplayedName, viewModel.DisplayedName);
+            Assert.False(viewModel.IsFavorite);
+        }
+    }
+}
